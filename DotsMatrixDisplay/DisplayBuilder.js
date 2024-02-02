113b05d0b61ca25b0aa0b86700a805ad6f07406b
fplath <frithjof_benjamin.plath@smail.th-koeln.de>
2024-02-02 11:24:13 +0100

    Turned DisplayBuilder.js into class and make it work with sketch.js

--- DotsMatrixDisplay/DisplayBuilder.js
+++ DotsMatrixDisplay/DisplayBuilder.js
@@ -1,140 +1,152 @@
-
-let dataHandler;
-let columnDummy = [5,17,5];
-let x = 0;
-let lastUpdateTime = 0;
-function preload() {
-    Constants.bitmapFont = loadJSON('BitMapCharSet.json');
-    dataHandler = new DataHandler();
-    // add data here
-    dataHandler.setTrainInfoData(loadJSON('staticKvBData.json'));
-}
-
-function setup() {
-    setupDisplay();
-}
-
-function draw() {
-    DisplayDrawer.displayDraw();
-    updatingForAnimation();
-}
-
-function setupDisplay() {
-
-    Constants.canvasWidth = Constants.led *(Constants.marginLeftRightDots*2 + Constants.columnWidthDots*(columnDummy[0]+columnDummy[1]+columnDummy[2]) + Constants.textBoxSpacingDots*2);
-    createCanvas(Constants.canvasWidth, Constants.canvasHeight);
-    for (let i = 0; i < Constants.contentRowCount; i++){
-        createRow(dataHandler.getTrainInfoArray()[i]);
+class DisplayBuilder {
+    constructor(dataHandler, columnArray) {
+        this.dataHandler = dataHandler;
+        this.columns = columnArray;
+        this.x = 0;
+        this.lastUpdateTime = 0;
     }
-    background(0);
-}
 
-function updatingForAnimation(){
-    let currentTime = millis();
-    if (currentTime - lastUpdateTime >= Constants.displayUpdatingRate) {
-        x += 1;
-        lastUpdateTime = currentTime;
+    setupDisplay() {
+        Constants.canvasWidth = Constants.led * (Constants.marginLeftRightDots * 2 + Constants.columnWidthDots * (this.columns[0] + this.columns[1] + this.columns[2]) + Constants.textBoxSpacingDots * 2);
+        createCanvas(Constants.canvasWidth, Constants.canvasHeight);
+        for (let i = 0; i < Constants.contentRowCount; i++) {
+            this.createRow(dataHandler.getTrainInfoArray()[i]);
+        }
+        background(0);
     }
-}
+
+
+    displayDraw() {
+        let y = Constants.ledSpacing;
+        for (const element of Constants.fullRows) {
+            for (const row of this.createRowOutput(element)) {
+                let x = Constants.ledSpacing;
+                for (const char of row) {
+                    if (char === '1') {
+                        fill(Constants.colorOn); // Turn on the LED
+
+                    } else {
+                        fill(Constants.colorOff); // Turn off the LED
+                    }
+                    noStroke();
+                    square(x, y, Constants.ledLampSize);
+                    //ellipseMode(CORNER);
+                    //circle(x, y, Constants.ledLampSize);
+                    x += Constants.led;
+                }
+                y+= Constants.led;
+            }
+            //console.log(frameRate());
+        }
+    }
+    updatingForAnimation() {
+        let currentTime = millis();
+        if (currentTime - this.lastUpdateTime >= Constants.displayUpdatingRate) {
+            this.x += 1;
+            this.lastUpdateTime = currentTime;
+        }
+    }
 
 // Function to get the bit pattern for a character
-function getCharBitPattern(char) {
-    const defaultPattern = ["00000", "00000", "00000", "00000", "00000", "00000", "00000"];
-    return Constants.bitmapFont[char] || defaultPattern;
-}
 
 
-function createRowOutput(rowArray) {
-    let resultRows = Array(7).fill('');
-    let columnSpacing = '';
-    if (rowArray.length !== 1) {
-        for (let i = 0; i < Constants.columnWidthDots; i++) {
+    getCharBitPattern(char) {
+        const defaultPattern = ["00000", "00000", "00000", "00000", "00000", "00000", "00000"];
+        return Constants.bitmapFont[char] || defaultPattern;
+    }
+    createRowOutput(rowArray) {
+        let resultRows = Array(7).fill('');
+        let columnSpacing = '';
+        if (rowArray.length !== 1) {
+            for (let i = 0; i < Constants.columnWidthDots; i++) {
+                columnSpacing += '0';
+            }
+            for (let i = 0; i < rowArray.length; i++) {
+                resultRows = resultRows.map((row, idx) => row + this.trimRows(rowArray[i][idx], this.columns[i] * (Constants.columnWidthDots))); //Breite der Textbox
+                if (i < rowArray.length - 1) {
+                    resultRows = resultRows.map(row => row + columnSpacing);
+                }
+            }
+            let margin = '';
+            for (let i = 0; i < Constants.marginLeftRightDots; i++) {
+                margin += '0';
+            }
+            resultRows = resultRows.map(row => margin + row + margin);
+            return resultRows;
+        } else {
+            return rowArray[0];
+        }
+
+    }
+
+    createBlankRows() {
+        let blankRows = Array(Constants.spaceBetweenRowsDots).fill('');
+        for (let i = 0; i < Constants.spaceBetweenRowsDots; i++) {
+            blankRows[i] = '0'.repeat(Constants.canvasWidth);
+        }
+        let arrayAroundBlankRows = [];
+        arrayAroundBlankRows.push(blankRows);
+        Constants.fullRows.push(arrayAroundBlankRows);
+    }
+
+    createRow(stringArray) {
+        let resultRows = Array(7).fill('');
+        let fullRow = [];
+        let columnSpacing = '';
+        for (let i = 0; i < Constants.textBoxSpacingDots; i++) {
             columnSpacing += '0';
         }
-        for (let i = 0; i < rowArray.length; i++) {
-            resultRows = resultRows.map((row, idx) => row + trimRows(rowArray[i][idx],columnDummy[i]*(Constants.columnWidthDots))); //Breite der Textbox
-            if (i < rowArray.length -1) {
-                resultRows = resultRows.map(row => row + columnSpacing);
-            }
+        for (let i = 0; i < stringArray.length; i++) {
+            let textCell = this.createTextCell(stringArray[i]);
+            resultRows = resultRows.map((row, idx) => row + textCell[idx]);
+            fullRow.push(resultRows);
+            resultRows = Array(7).fill('');
         }
-        let margin = '';
-        for (let i = 0; i < Constants.marginLeftRightDots; i++) {
-            margin += '0';
+        Constants.fullRows.push(fullRow);
+        this.createBlankRows(); // create a line spacing
+    }
+
+    createTextCell(str) {
+        let resultRows = Array(7).fill('');
+        for (let char of str) {
+            let pattern = this.getCharBitPattern(char.toUpperCase());
+            pattern.forEach((row, idx) => {
+                let distanceToNextChar = '';
+                for (let i = 0; i < Constants.spacesBetweenCharsDots; i++) {
+                    distanceToNextChar += '0';
+                }
+                resultRows[idx] += row + distanceToNextChar;
+            });
         }
-        resultRows = resultRows.map(row => margin + row + margin );
         return resultRows;
-    } else {
-        return rowArray[0];
     }
 
-}
+    trimRows(row, width) {
+        let zeros = '';
+        for (let i = 0; i < width - row.length; i++) {
+            zeros += '0';
+        }
+        row = row + zeros;
+        if (row.length <= width) {
+            return row
+        }
+        row = this.animateTextInCell(row).slice(0, width);
+        return row;
+    }
 
-function createBlankRows() {
-    let blankRows = Array(Constants.spaceBetweenRowsDots).fill('');
-    for (let i = 0; i < Constants.spaceBetweenRowsDots; i++) {
-        blankRows[i] = '0'.repeat(Constants.canvasWidth);
-    }
-    let arrayAroundBlankRows = [];
-    arrayAroundBlankRows.push(blankRows);
-    Constants.fullRows.push(arrayAroundBlankRows);
-}
+    animateTextInCell(row) {
+        let zeros = '';
 
-function createRow(stringArray) {
-    let resultRows = Array(7).fill('');
-    let fullRow = [];
-    let columnSpacing = '';
-    for (let i = 0; i < Constants.textBoxSpacingDots; i++) {
-        columnSpacing += '0';
-    }
-    for (let i = 0; i < stringArray.length; i++) {
-        let textCell = createTextCell(stringArray[i]);
-        resultRows = resultRows.map((row, idx) => row + textCell[idx]);
-        fullRow.push(resultRows);
-        resultRows = Array(7).fill('');
-    }
-    Constants.fullRows.push(fullRow);
-    createBlankRows(); // create a line spacing
-}
+        for (let i = 0; i < (Constants.columnWidthDots); i++) {
+            zeros += '0';
+        }
+        let newRow = row + zeros;
 
-function createTextCell(str) {
-    let resultRows = Array(7).fill('');
-    for (let char of str) {
-        let pattern = getCharBitPattern(char.toUpperCase());
-        pattern.forEach((row, idx) => {
-            let distanceToNextChar ='';
-            for (let i = 0; i < Constants.spacesBetweenCharsDots; i++) {
-                distanceToNextChar += '0';
-            }
-            resultRows[idx] += row + distanceToNextChar;
-        });
-    }
-    return resultRows;
-}
+        let offset = this.x % newRow.length;
 
-function trimRows(row, width) {
-    let zeros = '';
-    for (let i = 0; i < width - row.length ; i++) {
-        zeros += '0';
+        if (this.x > 2147483647) { // Reset if x get to big ?
+            this.x = 0;
+        }
+        return newRow.slice(offset) + newRow.substring(0, offset);
     }
-    row = row + zeros;
-    if (row.length <= width) {
-        return row
-    }
-    row = animateTextInCell(row).slice(0, width);
-    return row;
-}
-function animateTextInCell(row){
-    let zeros = '';
-
-    for (let i = 0; i < (Constants.columnWidthDots); i++) {
-        zeros += '0';
-    }
-    let newRow = row + zeros;
-
-    let offset = x % newRow.length;
-
-    if (x > 2147483647) { // Reset if x get to big ?
-        x = 0;
-    }
-    return newRow.slice(offset) + newRow.substring(0, offset);
 }
--- DotsMatrixDisplay/sketch.js
+++ DotsMatrixDisplay/sketch.js
@@ -1,42 +1,39 @@
-/*let bitmapFont;
 let dataHandler;
+let displayBuilder;
+let dataSource;
 
 function preload() {
-  bitmapFont = loadJSON('BitMapCharSet.json');
+  Constants.bitmapFont = loadJSON('BitMapCharSet.json');
   dataHandler = new DataHandler();
-  // add data here
+  // --- add data here
   dataHandler.setTrainInfoData(loadJSON('staticKvBData.json'));
+  // ---
+  displayBuilder = new DisplayBuilder(dataHandler,[5,17,6]);
 }
-
-let fahrplanLayouter = new Layouter([5,24,6]);
-let calendarLayouter = new Layouter([4,24,8]);
 
 
 
 function setup() {
-  Constants.canvasWidth = fahrplanLayouter.getTotalWidth();
-  createCanvas(Constants.canvasWidth, Constants.canvasHeight);
-  background(0);
-
+    displayBuilder.setupDisplay();
   // ####### ADD DATA HERE #######
-  fahrplanLayouter.addDataArray(dataHandler.getTrainInfoArray());
-  calendarLayouter.addDataArray(dataHandler.getCalendarArray());
 
   dataSource = true;
 
 }
 function draw() {
   background(0);
-  fahrplanLayouter.footerText = "Diese Woche kümmern sich Hendrik und Jakob um die Ordnung hier ** Unser Tolleyball-Grand-Champion ist ... trommelwirbel ... Jan!! ** so langsam lässt die Kreativleistung nach, der Kafeestand müsste nachgefüllt werden";
+  displayBuilder.displayDraw();
+  displayBuilder.updatingForAnimation();
+  //fahrplanLayouter.footerText = "Diese Woche kümmern sich Hendrik und Jakob um die Ordnung hier ** Unser Tolleyball-Grand-Champion ist ... trommelwirbel ... Jan!! ** so langsam lässt die Kreativleistung nach, der Kafeestand müsste nachgefüllt werden";
   // fahrplanLayouter.setHeaderText("Abfahrtsplan");
-  if (dataSource) {
+  /*if (dataSource) {
     fahrplanLayouter.draw();
   } else {
     calendarLayouter.draw();
-  }
+  }*/
 }
 function keyPressed() {
   if (key === 'x' || key === 'X') {
     dataSource = !dataSource;
   }
-} */
+}
